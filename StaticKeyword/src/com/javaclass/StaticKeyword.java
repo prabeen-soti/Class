df36c69c25365c90fddaf4700c4c4fb5a8972c9c
prabhaw <mail.prabhaw@gmail.com>
2020-02-06 20:56:02 +0545

    2020-02-06

--- StaticKeyword/src/com/javaclass/StaticKeyword.java
+++ StaticKeyword/src/com/javaclass/StaticKeyword.java
@@ -8,23 +8,23 @@ public class StaticKeyword {
 
 	public static void main(String[] args) {
 
-//		Student.college = "KEC";
+		Student.college = "KEC";
 		
 //		Student.display(); not static canot called this way
 	
 		
 		Student std = new Student(1,"Bimal");
-//		Student std1 = new Student(2,"Sagar");
-//		
+		Student std1 = new Student(2,"Sagar");
+		
 		std.display();
 		
-//		Student.change();
-//		
-//		Student.college = "ABC";
-//		Student std2 = new Student(3,"Chandra");
-//		std.display();
-//		std1.display();
-//		std2.display();
+		Student.change();
+		
+		Student.college = "ABC";
+		Student std2 = new Student(3,"Chandra");
+		std.display();
+		std1.display();
+		std2.display();
 	}
 
 }
